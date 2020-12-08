42e73e0d76073ab424e2390902ef75fd1e2db5e9
Sahil Kumar <54834362+Sahil4UI@users.noreply.github.com>
2020-12-08 20:14:12 +0530

    Create Operators.java

---
+++ Operators.java
@@ -0,0 +1,35 @@
+
+public class Operators {
+	public static void main(String[] args) {
+//		comparison operators > , < , >= , <=,==(equality operator), !=(not equals to)
+//		int a = 10;
+//		int b = 10;
+//		System.out.println(a>b);
+//		System.out.println(a<b);
+//		System.out.println(a>=b);
+//		System.out.println(a<=b);
+//		System.out.println(a==b);
+//		System.out.println(a!=b);
+		
+//		logical Operator -and , or , not
+//		and(&&)-all conditions must be true
+//		System.out.println(10 < 20 && 20 >=20 && 30 < 100);
+//		or(||) - atleast one condition must be true
+//		System.out.println(10 == 20 || 20 >200 || 30 < 100);
+//		not(!) -> true->false , false > true
+//		System.out.println(!(10 == 20 || 20 >200 || 30 < 100) );
+		
+//		Bitwise operator - bitwise and(&)
+//		System.out.println(10 & 25);
+//		bitwise or (|)
+//		System.out.println(98 | 20);
+		
+//		bitwise -->>>> left-shift & right shift operators
+//		System.out.println(20 << 2); //left shift
+//		System.out.println(25 >> 2); //right shift
+		
+//		bitwise not->~
+//		System.out.println(~-5);
+		
+	}
+}
