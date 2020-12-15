f435b53a933451c50ced0daf4e960e34b5ccf1b7
Sahil Kumar <54834362+Sahil4UI@users.noreply.github.com>
2020-12-15 21:11:27 +0530

    Create Nested.java

---
+++ Nested.java
@@ -0,0 +1,118 @@
+class Nested{
+    public static void main(String[] args) {
+        // for (int i =1;i<=3;i++)
+        // {
+        //     for(int j=1;j<=3;j++)
+        //     {
+        //         System.out.println("i="+i+"j="+j);
+                
+        //     }
+        // }
+
+        // 1
+        // 12
+        // 123
+        // 1234
+        // 12345
+
+        // for(int i =1;i<=5;i++)
+        // {
+        //     for (int k=1;k<=5-i;k++)
+        //     {
+        //         System.out.print(" ");
+
+        //     }
+        //     for(int j=1;j<=i;j++)
+        //     {
+        //         System.out.print("* ");
+        //     }
+        //     System.out.println();
+
+        // }
+        // int x = 1;
+        // for(int i =1;i<=4;i++)
+        // {
+        //     for(int j=1;j<=i;j++)
+        //     {
+        //         System.out.print(x);
+        //         x++;
+        //     }
+        //     System.out.println();
+
+        // }
+
+        // 1
+        // 10
+        // 101
+        // 1010
+        // 10101
+        
+        // for(int i=1;i<=5;i++)
+        // {
+        //     for(int j=1;j<=i;j++)
+        //     {
+        //         if (i%2==0)
+        //         {
+        //             System.out.print(0);
+        //         }
+        //         else{
+        //             System.out.print(1);
+        //         }
+        //     }
+        //     System.out.println();
+        // }
+        // int x=1;
+        // for(int i=1;i<=5;i++)
+        // {
+        //     for(int j=1;j<=i;j++)
+        //     {
+        //         if (x%2==0)
+        //         {
+        //             System.out.print(0);
+        //         }
+        //         else{
+        //             System.out.print(1);
+        //         }
+        //         x++;
+        //     }
+        //     System.out.println();
+        // }
+        // 1
+        // 01
+        // 010
+        // 1010
+        // 10101
+        // int isPrime=1;
+        // for(int i=2;i<=100;i++)
+        // {
+        //     for(int j=2;j<=i-1;j++)
+        //     {
+        //         if (i%j==0)
+        //         {
+        //             isPrime=0;
+        //             break;
+        //         }
+        //         else{
+        //             isPrime=1;
+        //         }
+
+        //     }
+        //     if (isPrime==1)
+        //     {
+        //         System.out.println(i);
+        //     }
+
+        // for (int i=1;i<=10;i++)
+        // {
+        //     System.out.println(i);
+        // }
+
+        int x =1;
+        while (x<=10)
+        {
+            System.out.println(x);
+            // x++;
+        }
+        
+        }
+    }
